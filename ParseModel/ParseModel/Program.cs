dbbb513ed0eb79c3a154a62a16c7aee59a176fbe
Ekarat Rattagan <pokekarat@gmail.com>
2013-12-30 11:31:47 +0800

    SRC V5

--- ParseModel/ParseModel/Program.cs
+++ ParseModel/ParseModel/Program.cs
@@ -6,6 +6,7 @@ using NCalc;
 using System.Diagnostics;
 using System.IO;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace ParseModel
 {
@@ -19,15 +20,48 @@ namespace ParseModel
 
             string[] testFile = new string[numOfTest];
             string[] model = File.ReadAllLines(modelFile);
+            string input = model[0].Replace("sin", "Sin")
+                .Replace("cos", "Cos");
 
+            ArrayList al = new ArrayList();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/' || input[i] == '%')
+                {
+                    al.Add(input[i]);
+                }
+            }
+
+            string[] sArray = input.Split(new char[5]{'+','-','*','/','%'});
+            float offset = float.Parse(sArray[0]);
+
+            string equation = "";
+            for (int i = 0; i < al.Count; i++)
+            {
+                if (sArray[i].Contains('^'))
+                {
+                    //Console.WriteLine("modify");
+                    string[] pow = sArray[i].Split('^');
+                    equation += "Pow(" + pow[0] + "," + pow[1] + ")" + al[i];
+                }
+                else
+                {
+                    equation += sArray[i] + " " + al[i];
+                }
+            }
+
+            equation += sArray[sArray.Length - 1];
+            float avgEnergy = 0;
+            TextWriter tw = new StreamWriter(rootPath + "energy.txt");
             for (int n = 0; n < numOfTest; n++)
             {
                 
                 string[] currFile = File.ReadAllLines(rootPath+(n+1)+@"\test.txt");
                 string[] varNames = currFile[0].Split('\t');
 
-                Expression e = new Expression(model[0]);
-                
+                Expression e = new Expression(equation);
+                float energy = 0;
+
                 for (int i = 1; i < currFile.Length; i++)
                 {
 
@@ -46,6 +80,7 @@ namespace ParseModel
                     try
                     {
                         Console.WriteLine(e.Evaluate());
+                        energy += float.Parse(e.Evaluate().ToString())/1000;
                     }
                     catch (EvaluationException ee)
                     {
@@ -53,9 +88,18 @@ namespace ParseModel
                     }
 
                 }
+                Console.WriteLine("Test " + (n+1) + " = " + energy);
+                tw.WriteLine("Test " + (n+1) + " = " + energy);
+                //Console.ReadKey();
+                avgEnergy += energy;
             }
+
+            Console.WriteLine("average energy = " + (avgEnergy / 5));
+            //Console.ReadKey();
+
            
-            Console.ReadKey();
+            tw.WriteLine("Average energy " + avgEnergy / 5 + " joules");
+            tw.Close();
         }
     }
 }
